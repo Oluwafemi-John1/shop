861f80358fcc95b14041cf67a0576a268c9f35dd
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-03-13 15:51:21 +0100

    feat: replace MDB UI Kit with Bootstrap for styling and update Navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
-// import './App.css'
+import './App.css'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Landingpage from './pages/Landingpage'
-import * as mdb from 'mdb-ui-kit'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 function App() {
