d22f5daaea0be7880f4d0fd78c6a0106bf770ca0
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-03-15 14:59:09 +0100

    feat: implement AI content generation endpoint with prompt validation and error handling

--- server/index.js
+++ server/index.js
@@ -1,24 +1,33 @@
 import express from 'express'
 import env from 'dotenv'
 import { GoogleGenerativeAI } from '@google/generative-ai'
+import bodyParser from 'body-parser'
+
 const app = express()
 env.config()
+app.use(bodyParser.json())
+
+
 const port = process.env.PORT
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY
-
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 
 
-
-app.get('/ai', async (req, res) => {
-    const prompt = "Explain how AI works";
-    
-    const result = await model.generateContent(prompt);
-    console.log(re);
-    
-    console.log(result.response.text());
+app.post('/ai', async (req, res) => {
+    const {prompt} = req.body;
+    if(!prompt) {
+        return res.status(400).send("Prompt is required")
+    }
+    try {
+        const result = await model.generateContent(prompt);
+        console.log('request sent');
+        res.status(200).json({response: result.response.text()})
+    } catch(error) {
+        console.error(error)
+        res.status(500).send('An error occurred')
+    }
 })
 
 
