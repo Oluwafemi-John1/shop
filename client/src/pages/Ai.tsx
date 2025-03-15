50c8696ccf871657159e98af32033d3eb91c61af
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-03-15 15:24:04 +0100

    feat: implement AI prompt submission with state management and CORS support

--- client/src/pages/Ai.tsx
+++ client/src/pages/Ai.tsx
@@ -1,17 +1,29 @@
 import axios from "axios"
 import Navbar from "../components/Navbar"
+import { useState } from "react"
 
 const Ai = () => {
-    const endpoint: string = 'http://localhost:5453'
-    axios.get(endpoint)
+    const [prompt, setprompt] = useState('')
+    const payload = {prompt}
+    const endpoint: string = 'http://localhost:5453/ai'
+
+    const getResponse = () => {
+        axios.post(endpoint, payload)
+        .then((res)=>{
+            console.log(res.data);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
 
     return (
         <>
             <Navbar />
             <div className="col-lg-7 col-10 mx-auto p-lg-4 p-2 shadow mt-lg-4 mt-2 rounded-2">
                 <h3 className="text-center my-3">Gen AI</h3>
-                <input type="text" className="form-control my-2 form-control-sm shadow-none" placeholder="Enter prompt" />
-                <button className="btn btn-dark btn-sm w-100 my-2 fw-bold">Make Prompt</button>
+                <input type="text" className="form-control my-2 form-control-sm shadow-none" name='prompt' value={prompt} placeholder="Enter prompt" onChange={(e)=>{setprompt(e.target.value)}} />
+                <button onClick={getResponse} className="btn btn-dark btn-sm w-100 my-2 fw-bold">Make Prompt</button>
             </div>
         </>
     )
--- server/index.js
+++ server/index.js
@@ -2,11 +2,17 @@ import express from 'express'
 import env from 'dotenv'
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import bodyParser from 'body-parser'
+import cors from 'cors'
+
 
 const app = express()
 env.config()
 app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
+
+app.use(cors())
 
 const port = process.env.PORT
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY
@@ -16,15 +22,18 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 
 app.post('/ai', async (req, res) => {
-    const {prompt} = req.body;
-    if(!prompt) {
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    const { prompt } = req.body;
+    console.log(prompt);
+
+    if (!prompt) {
         return res.status(400).send("Prompt is required")
     }
     try {
         const result = await model.generateContent(prompt);
         console.log('request sent');
-        res.status(200).json({response: result.response.text()})
-    } catch(error) {
+        res.status(200).json({ response: result.response.text() })
+    } catch (error) {
         console.error(error)
         res.status(500).send('An error occurred')
     }
